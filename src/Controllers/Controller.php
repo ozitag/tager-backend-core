c78d59ab4f873a56ce3abbb4e0869c6d5ddeeb8d
Vital Ozierski <v.ozierski@ozitag.com>
2020-07-23 22:55:59 +0300

    1.2.1 - support array configuration of features in Controller

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -13,6 +13,41 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, MarshalTrait;
 
+    protected function features()
+    {
+        return [];
+    }
+
+    private function serveFeatureByKey($key, $params = [])
+    {
+        $features = $this->features();
+
+        if (!isset($features[$key])) {
+            throw new \Exception('Feature not found');
+        }
+
+        $feature = $features[$key];
+
+        if (is_string($feature)) {
+            return $this->serve($feature, $params);
+        } else if (is_array($feature)) {
+            $featureName = array_shift($feature);
+
+            $reflection = new \ReflectionClass($featureName);
+            $constructorParams = $reflection->getConstructor()->getParameters();
+
+            $featureParams = $params;
+            foreach ($constructorParams as $ind => $param) {
+                if ($ind < count($params)) continue;
+                $featureParams[$param->getName()] = $feature[$ind - count($params)];
+            }
+
+            return $this->serve($featureName, $featureParams);
+        } else {
+            return $this->dispatchNow($feature);
+        }
+    }
+
     /**
      * Serve the given feature with the given arguments.
      *
@@ -22,8 +57,12 @@ class Controller extends BaseController
      * @return mixed
      * @throws \ReflectionException
      */
-    public function serve($feature, $arguments = [])
+    protected function serve($feature, $arguments = [])
     {
+        if (!class_exists($feature)) {
+            return $this->serveFeatureByKey($feature);
+        }
+
         return $this->dispatchNow($this->marshal($feature, new Collection(), $arguments));
     }
 }
