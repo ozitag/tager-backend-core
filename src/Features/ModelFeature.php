c6e0e6d5d620876f462d876d884f54d278beb512
Vital Ozierski <v.ozierski@ozitag.com>
2020-07-24 10:50:33 +0300

    1.2.2 - add repository parameter for ModelFeature, not jobGetByIdClass is not required

--- src/Features/ModelFeature.php
+++ src/Features/ModelFeature.php
@@ -2,22 +2,35 @@
 
 namespace OZiTAG\Tager\Backend\Core\Features;
 
+use OZiTAG\Tager\Backend\Core\Repositories\EloquentRepository;
+
 class ModelFeature extends Feature
 {
+    /** @var string */
     private $jobGetByIdClass;
 
+    /** @var EloquentRepository */
+    private $repository;
+
+    /** @var integer */
     protected $id;
 
-    public function __construct($id, $jobGetByIdClass)
+    public function __construct($id, $jobGetByIdClass, ?EloquentRepository $repository = null)
     {
         $this->id = $id;
 
         $this->jobGetByIdClass = $jobGetByIdClass;
+
+        $this->repository = $repository;
     }
 
     protected function model()
     {
-        $model = $this->run($this->jobGetByIdClass, ['id' => $this->id]);
+        if ($this->jobGetByIdClass) {
+            $model = $this->run($this->jobGetByIdClass, ['id' => $this->id]);
+        } else {
+            $model = $this->repository->find($this->id);
+        }
 
         if (!$model) {
             abort(404, 'Not Found');
