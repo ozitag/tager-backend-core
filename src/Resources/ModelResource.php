bce24b4768e01c73cb82c391395adba42ee11581
Vital Ozierski <v.ozierski@ozitag.com>
2020-07-25 12:44:11 +0300

    support relations in ModelResource

--- src/Resources/ModelResource.php
+++ src/Resources/ModelResource.php
@@ -2,6 +2,7 @@
 
 namespace OZiTAG\Tager\Backend\Core\Resources;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Ozerich\FileStorage\Models\File;
 
@@ -28,10 +29,53 @@ abstract class ModelResource extends JsonResource
         }
     }
 
-    private function parseField($field)
+    private function getRelationValue($field)
     {
+        if (!isset($field['relation'])) {
+            throw new \Exception('Relation is not set');
+        }
+
+        $relation = $field['relation'];
+        $value = $this->{$relation};
+
+        if (!isset($field['as'])) {
+            throw new \Exception('As not set');
+        }
+        $as = $field['as'];
+
+        if (!is_array($as)) {
+            if ($value instanceof Collection) {
+                $result = [];
+                foreach ($value as $item) {
+                    $result[] = $this->parseField($field['as'], $item);
+                }
+                return $result;
+            } else {
+                return $this->parseField($field['as'], $value);
+            }
+        }
+
+        if ($value instanceof Collection) {
+            $result = [];
+            foreach ($value as $item) {
+                $result[] = $this->parseArray($as, $item);;
+            }
+            return $result;
+        }
+
+        return $this->parseArray($as, $value);
+    }
+
+    private function parseField($field, $model = null)
+    {
+        $model = is_null($model) ? $this : $model;
+
+        if (is_array($field)) {
+            return $this->getRelationValue($field);
+        }
+
         $fieldParams = explode(':', $field);
-        $value = $this->{$fieldParams[0]};
+        $value = $model->{$fieldParams[0]};
 
         if (count($fieldParams) > 1) {
             if ($fieldParams[1] == 'file') {
@@ -42,11 +86,11 @@ abstract class ModelResource extends JsonResource
         return $value;
     }
 
-    public function toArray($request)
+    private function parseArray($fields, $model)
     {
         $result = [];
 
-        foreach ($this->fields() as $field => $fieldData) {
+        foreach ($fields as $field => $fieldData) {
             if (is_numeric($field)) {
                 $field = $fieldData;
             }
@@ -55,9 +99,14 @@ abstract class ModelResource extends JsonResource
                 $field = mb_substr($field, 0, mb_strpos($field, ':'));
             }
 
-            $result[$field] = $this->parseField($fieldData);
+            $result[$field] = $this->parseField($fieldData, $model);
         }
 
         return $result;
+    }
+
+    public function toArray($request)
+    {
+        return $this->parseArray($this->fields(), $this);
     }
 }
