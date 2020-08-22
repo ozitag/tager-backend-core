b5d1d78361eeb09898304c9ab3b2e0fc30d477b9
Vital Ozierski <v.ozierski@ozitag.com>
2020-08-22 11:50:13 +0300

    add 'count' action to resources

--- src/Routing/ApiResource.php
+++ src/Routing/ApiResource.php
@@ -5,12 +5,32 @@ namespace OZiTAG\Tager\Backend\Core\Routing;
 use Illuminate\Routing\Router;
 
 class ApiResource {
-    
+
     public static function register()
     {
         if (!Router::hasMacro('apiResourceWithMove')) {
             Router::macro('apiResourceWithMove', function ($name, $controller, $options = []) {
                 $only = ['index', 'show', 'store', 'update', 'destroy', 'move'];
+                if (isset($options['except'])) {
+                    $only = array_diff($only, (array) $options['except']);
+                }
+                Router::resource($name, $controller, array_merge(['only' => $only], $options));
+            });
+        }
+
+        if (!Router::hasMacro('apiResourceWithMoveCount')) {
+            Router::macro('apiResourceWithMoveCount', function ($name, $controller, $options = []) {
+                $only = ['index', 'show', 'store', 'update', 'destroy', 'move', 'count'];
+                if (isset($options['except'])) {
+                    $only = array_diff($only, (array) $options['except']);
+                }
+                Router::resource($name, $controller, array_merge(['only' => $only], $options));
+            });
+        }
+
+        if (!Router::hasMacro('apiResourceWithCount')) {
+            Router::macro('apiResourceWithCount', function ($name, $controller, $options = []) {
+                $only = ['index', 'show', 'store', 'update', 'destroy', 'count'];
                 if (isset($options['except'])) {
                     $only = array_diff($only, (array) $options['except']);
                 }
--- src/Routing/ResourceRegistrar.php
+++ src/Routing/ResourceRegistrar.php
@@ -4,42 +4,61 @@ namespace OZiTAG\Tager\Backend\Core\Routing;
 
 use Illuminate\Routing\ResourceRegistrar as BaseResourceRegistrar;
 
-class ResourceRegistrar extends BaseResourceRegistrar {
+class ResourceRegistrar extends BaseResourceRegistrar
+{
+    protected $resourceDefaults = ['index', 'store', 'show', 'update', 'destroy', 'move', 'count'];
 
-    protected $resourceDefaults = ['index', 'store', 'show', 'update', 'destroy', 'move'];
     /**
      * Add the index method for a resourceful route.
      *
-     * @param  string  $name
-     * @param  string  $base
-     * @param  string  $controller
-     * @param  array  $options
+     * @param string $name
+     * @param string $base
+     * @param string $controller
+     * @param array $options
      * @return \Illuminate\Routing\Route
      */
     protected function addResourceMove($name, $base, $controller, $options)
     {
-        $uri = $this->getResourceUri($name).'/{'.$base.'}'.'/move/{direction}';
+        $uri = $this->getResourceUri($name) . '/{' . $base . '}' . '/move/{direction}';
 
         $action = $this->getResourceAction($name, $controller, 'move', $options);
 
         return $this->router->post($uri, $action);
     }
 
+    /**
+     * Add the count method for a resourceful route.
+     *
+     * @param string $name
+     * @param string $base
+     * @param string $controller
+     * @param array $options
+     * @return \Illuminate\Routing\Route
+     */
+    protected function addResourceCount($name, $base, $controller, $options)
+    {
+        $uri = $this->getResourceUri($name) . '/count';
+
+        $action = $this->getResourceAction($name, $controller, 'count', $options);
+
+        return $this->router->get($uri, $action);
+    }
+
 
     /**
      * Add the update method for a resourceful route.
      *
-     * @param  string  $name
-     * @param  string  $base
-     * @param  string  $controller
-     * @param  array  $options
+     * @param string $name
+     * @param string $base
+     * @param string $controller
+     * @param array $options
      * @return \Illuminate\Routing\Route
      */
     protected function addResourceUpdate($name, $base, $controller, $options)
     {
         $name = $this->getShallowName($name, $options);
 
-        $uri = $this->getResourceUri($name).'/{'.$base.'}';
+        $uri = $this->getResourceUri($name) . '/{' . $base . '}';
 
         $action = $this->getResourceAction($name, $controller, 'update', $options);
 
@@ -50,17 +69,17 @@ class ResourceRegistrar extends BaseResourceRegistrar {
     /**
      * Add the update method for a resourceful route.
      *
-     * @param  string  $name
-     * @param  string  $base
-     * @param  string  $controller
-     * @param  array  $options
+     * @param string $name
+     * @param string $base
+     * @param string $controller
+     * @param array $options
      * @return \Illuminate\Routing\Route
      */
     protected function addResourceDestroy($name, $base, $controller, $options)
     {
         $name = $this->getShallowName($name, $options);
 
-        $uri = $this->getResourceUri($name).'/{'.$base.'}';
+        $uri = $this->getResourceUri($name) . '/{' . $base . '}';
 
         $action = $this->getResourceAction($name, $controller, 'delete', $options);
 
@@ -70,17 +89,17 @@ class ResourceRegistrar extends BaseResourceRegistrar {
     /**
      * Add the show method for a resourceful route.
      *
-     * @param  string  $name
-     * @param  string  $base
-     * @param  string  $controller
-     * @param  array  $options
+     * @param string $name
+     * @param string $base
+     * @param string $controller
+     * @param array $options
      * @return \Illuminate\Routing\Route
      */
     protected function addResourceShow($name, $base, $controller, $options)
     {
         $name = $this->getShallowName($name, $options);
 
-        $uri = $this->getResourceUri($name).'/{'.$base.'}';
+        $uri = $this->getResourceUri($name) . '/{' . $base . '}';
 
         $action = $this->getResourceAction($name, $controller, 'view', $options);
 
