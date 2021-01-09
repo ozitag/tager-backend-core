cd121f5d89d0e5ca7dfeb25583ec17cd32dcaa1f
Vital Ozierski <v.ozierski@ozitag.com>
2021-01-09 12:19:03 +0300

    add $hasUUID and $defaultOrder for TModel

--- src/Models/TModel.php
+++ src/Models/TModel.php
@@ -2,10 +2,36 @@
 
 namespace OZiTAG\Tager\Backend\Core\Models;
 
-use \Illuminate\Database\Eloquent\Model as BaseModel;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model as BaseModel;
+use Illuminate\Support\Str;
 use OZiTAG\Tager\Backend\Core\Models\Traits\FilterableAttributes;
 
 class TModel extends BaseModel
 {
     use FilterableAttributes;
+
+    static $hasUUID = false;
+
+    static $defaultOrder = null;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        if (static::$hasUUID) {
+            static::creating(function ($model) {
+                $model->uuid = (string)Str::uuid();
+            });
+        }
+
+        if (static::$defaultOrder) {
+            $defaultOrderParts = explode(' ', static::$defaultOrder);
+            if (count($defaultOrderParts) == 2 && in_array(strtoupper($defaultOrderParts[1]), ['ASC', 'DESC'])) {
+                static::addGlobalScope('order', function (Builder $builder) use ($defaultOrderParts) {
+                    $builder->orderBy($defaultOrderParts[0], $defaultOrderParts[1]);
+                });
+            }
+        }
+    }
 }
