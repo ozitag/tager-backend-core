43882cf712db5f55ce0c5e5de849c78f609677b5
gennadijbakunovic <g.bakunovich@ozitag.com>
2021-02-04 12:58:38 +0300

    Change to Throwable

--- src/Traits/ExceptionHandler.php
+++ src/Traits/ExceptionHandler.php
@@ -2,40 +2,43 @@
 
 namespace OZiTAG\Tager\Backend\Core\Traits;
 
+use Closure;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Throwable;
 
 trait ExceptionHandler
 {
     /**
-     * @param \Closure $callback
+     * @param Closure $callback
      * @param string $logLevel
-     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     * @return Authenticatable|null
      */
-    protected function withExceptionHandler(\Closure $callback, string $logLevel = 'error')
+    protected function withExceptionHandler(Closure $callback, string $logLevel = 'error')
     {
         try {
             return $callback();
-        } catch (\Exception $exception) {
+        } catch (Throwable $exception) {
             return $this->logException($exception, $logLevel)
                 ->resolveException($exception);
         }
     }
 
     /**
-     * @param \Exception $exception
+     * @param Throwable $exception
      * @param string $logLevel
      * @return $this
      */
-    protected function logException(\Exception $exception, string $logLevel) {
+    protected function logException(Throwable $exception, string $logLevel) {
         Log::$logLevel([$exception->getMessage(), $exception->getFile(), $exception->getLine()]);
         return $this;
     }
 
     /**
-     * @param \Exception $exception
+     * @param Throwable $exception
      */
-    protected function resolveException(\Exception $exception) {
+    protected function resolveException(Throwable $exception) {
         throw new $exception;
     }
 }
