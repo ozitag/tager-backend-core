728baa42e8a1c6a986b5b1fdd3b0b568c7d2eedf
Vital Ozierski <v.ozierski@ozitag.com>
2021-02-24 02:01:59 +0300

    add static function labelList for all Enums

--- src/Enums/Enum.php
+++ src/Enums/Enum.php
@@ -4,5 +4,22 @@ namespace OZiTAG\Tager\Backend\Core\Enums;
 
 class Enum extends \BenSampo\Enum\Enum
 {
+    public static function label(string $value): string
+    {
+        return 'Unknown';
+    }
 
+    public static function labelList(): array
+    {
+        $data = [];
+
+        foreach (self::getValues() as $value) {
+            $data[] = [
+                'name' => $value,
+                'label' => self::label($value)
+            ];
+        }
+
+        return $data;
+    }
 }
