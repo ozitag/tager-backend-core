5407254c409c7e8bc853543fbaddc0feba0f8b39
Vital Ozierski <v.ozierski@ozitag.com>
2021-02-28 01:00:35 +0300

    maxPageSize can be -1 if max_int_value needed

--- src/Helpers/PaginationHelper.php
+++ src/Helpers/PaginationHelper.php
@@ -14,7 +14,7 @@ class PaginationHelper
             return 0;
         }
 
-        $current = (int) Request::get('pageNumber', 0);
+        $current = (int)Request::get('pageNumber', 0);
 
         if ($current < 1) {
             return 0;
@@ -25,7 +25,7 @@ class PaginationHelper
 
     public function perPage(): int
     {
-        $per_page = (int) Request::get($this->isOffsetBased() ? 'pageLimit' : 'pageSize', $this->defaultPageSize());
+        $per_page = (int)Request::get($this->isOffsetBased() ? 'pageLimit' : 'pageSize', $this->defaultPageSize());
 
         if ($per_page > $this->maxPageSize()) {
             return $this->defaultPageSize();
@@ -36,22 +36,31 @@ class PaginationHelper
 
     public function offset(): int
     {
-        return (int) Request::get('pageOffset', 0);
+        return (int)Request::get('pageOffset', 0);
     }
 
-    public function isOffsetBased(): bool {
+    public function isOffsetBased(): bool
+    {
         return Request::get('pageOffset', null) !== null;
     }
 
-    public function isPageBased(): bool {
+    public function isPageBased(): bool
+    {
         return Request::get('pageNumber', null) !== null;
     }
 
-    public function defaultPageSize() {
-        return (int) Config::get('tager-app.pagination.default_page_size');
+    public function defaultPageSize(): int
+    {
+        return (int)Config::get('tager-app.pagination.default_page_size');
     }
 
-    public function maxPageSize() {
-        return (int) Config::get('tager-app.pagination.max_page_size');
+    public function maxPageSize(): int
+    {
+        $result = (int)Config::get('tager-app.pagination.max_page_size');
+        if ($result == -1) {
+            return PHP_INT_MAX;
+        } else {
+            return $result;
+        }
     }
 }
