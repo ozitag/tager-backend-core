7e4c58c9b0980d78baf275e8e371abc0abf1ba60
Vital Ozierski <vital@ozerich.com>
2022-10-24 18:13:47 +0300

    1.8.12 - add $queryBuilder to ModelFeature

--- src/Features/ModelFeature.php
+++ src/Features/ModelFeature.php
@@ -2,30 +2,26 @@
 
 namespace OZiTAG\Tager\Backend\Core\Features;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use OZiTAG\Tager\Backend\Core\Repositories\EloquentRepository;
 
 class ModelFeature extends Feature
 {
-    protected ?string $jobGetByIdClass;
-
-    protected ?EloquentRepository $repository;
-
-    protected int $id;
-
-    public function __construct(int $id, ?string $jobGetByIdClass, ?EloquentRepository $repository = null)
+    public function __construct(
+        protected int                 $id,
+        protected ?string             $jobGetByIdClass,
+        protected ?EloquentRepository $repository = null,
+        protected ?Builder            $queryBuilder)
     {
-        $this->id = $id;
-
-        $this->jobGetByIdClass = $jobGetByIdClass;
-
-        $this->repository = $repository;
     }
 
     protected function model(): Model
     {
         if ($this->jobGetByIdClass) {
             $model = $this->run($this->jobGetByIdClass, ['id' => $this->id]);
+        } else if ($this->queryBuilder) {
+            $model = $this->queryBuilder->where('id', $this->id)->first();
         } else {
             $model = $this->repository->find($this->id);
         }
