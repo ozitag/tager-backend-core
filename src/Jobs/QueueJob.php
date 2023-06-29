8a91adf8ee3627834a1d7af72bfe55dd9ff13de6
Vital Ozierski <vital@ozerich.com>
2023-06-30 00:23:19 +0200

    1.9.11 - add run method to QueueJob

--- src/Jobs/QueueJob.php
+++ src/Jobs/QueueJob.php
@@ -11,4 +11,41 @@ use Illuminate\Queue\SerializesModels;
 class QueueJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+
+    protected static function getParameterValueForCommand($command, \ReflectionParameter $parameter, array $extras = [])
+    {
+        if (array_key_exists($parameter->name, $extras)) {
+            return $extras[$parameter->name];
+        }
+
+        if ($parameter->isDefaultValueAvailable()) {
+            return $parameter->getDefaultValue();
+        }
+
+        throw new \Exception("Unable to map parameter [{$parameter->name}] to command [{$command}]");
+    }
+
+    protected static function marshal($command, array $extras = [])
+    {
+        $injected = [];
+
+        $reflection = new \ReflectionClass($command);
+        if ($constructor = $reflection->getConstructor()) {
+            $injected = array_map(function ($parameter) use ($command, $extras) {
+                return self::getParameterValueForCommand($command, $parameter, $extras);
+            }, $constructor->getParameters());
+        }
+
+        return $reflection->newInstanceArgs($injected);
+    }
+
+    public function runNow($job, array $arguments = [])
+    {
+        if (!is_object($job)) {
+            $job = $this->marshal($job, $arguments);
+        }
+
+        return dispatch_sync($job);
+    }
 }
